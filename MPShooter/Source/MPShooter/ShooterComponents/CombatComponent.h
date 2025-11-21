a97d6244349a239390b91c7935dfb126212e11ea
Owen Beeks <91595654+OWENBEEKS@users.noreply.github.com>
2025-11-21 00:22:01 +0000

    Line trace for firing a projectile, + draw debug sphere at point of collision (temp crosshair)

--- MPShooter/Source/MPShooter/ShooterComponents/CombatComponent.h
+++ MPShooter/Source/MPShooter/ShooterComponents/CombatComponent.h
@@ -6,6 +6,8 @@
 #include "Components/ActorComponent.h"
 #include "CombatComponent.generated.h" // This include must appear last among all other includes
 
+#define TRACE_LENGTH 80000.f
+
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class MPSHOOTER_API UCombatComponent : public UActorComponent
 {
@@ -36,6 +38,8 @@ protected:
 	UFUNCTION(NetMulticast, Reliable)
 	void MulticastFire();
 
+	void TraceUnderCrosshairs(FHitResult& TraceHitResult);
+
 private:
 	class AMultiplayerCharacter* Character;
 
@@ -51,4 +55,6 @@ private:
 	float AimWalkSpeed;
 
 	bool bFireButtonPressed;
+
+	FVector HitTarget;
 };
