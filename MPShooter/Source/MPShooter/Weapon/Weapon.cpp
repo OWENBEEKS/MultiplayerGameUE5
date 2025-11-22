daba49cdc0e686b8695b2a7e65f02683b633267f
Owen Beeks <91595654+OWENBEEKS@users.noreply.github.com>
2025-11-22 19:27:37 +0000

    Added casing class and emitted

--- MPShooter/Source/MPShooter/Weapon/Weapon.cpp
+++ MPShooter/Source/MPShooter/Weapon/Weapon.cpp
@@ -7,6 +7,8 @@
 #include "MPShooter/Character/MultiplayerCharacter.h"
 #include "Net/UnrealNetwork.h"
 #include "Components/SkeletalMeshComponent.h"
+#include "Casing.h"
+#include "Engine/SkeletalMeshSocket.h"
 
 AWeapon::AWeapon()
 {
@@ -112,6 +114,24 @@ void AWeapon::Fire(const FVector& HitTarget)
 	if (FireAnimation)
 	{
 		WeaponMesh->PlayAnimation(FireAnimation, false);
+	}
+	if (CasingClass)
+	{
+		const USkeletalMeshSocket* AmmoEjectSocket = WeaponMesh->GetSocketByName(FName("AmmoEject"));
+		
+		if (AmmoEjectSocket)
+		{
+			FTransform SocketTransform = AmmoEjectSocket->GetSocketTransform(WeaponMesh);
+			UWorld* World = GetWorld();
+			if (World)
+			{
+				World->SpawnActor<ACasing>(
+					CasingClass,
+					SocketTransform.GetLocation(),
+					SocketTransform.GetRotation().Rotator()
+				);
+			}
+		}
 	}
 }
 
